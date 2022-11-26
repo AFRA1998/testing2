bd5a395144bf7d10c22f5d6bfe8f247d89667f4d
AFRA1998 <aframabrouk@gmail.com>
2022-11-26 15:22:26 +0100

    create main.c file

---
+++ LED.h
@@ -0,0 +1,13 @@
+#ifndef __LED_H
+#define __LED_H
+#include <stdint.h>
+
+#define GPIOD                   (uint32_t*)0x40020C00UL
+
+#define PIN15                   15u
+#define  PIN12                   12u
+
+ void ButtonToggleLed (void);
+ void ButtonToggleLed (void);
+#endif;
+ 
